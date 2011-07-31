166ee9934665201b658a0079f193321d3dfecd36
NegativeK <negativek+github@gmail.com>
2011-07-31 17:47:52 -0400

    Break out callbacks.

--- server/db.js
+++ server/db.js
@@ -4,6 +4,7 @@ var mongo = require("mongodb");
 function start(log) {
 	var db, user, password
 
+	// Herp derp conf file.
 	user = 'user';
 	password = 'password';
 	db = new mongo.Db('mud', new mongo.Server("127.0.0.1", 27017, 
@@ -13,19 +14,21 @@ function start(log) {
 		if(err) {
 			var logMsg;
 
-			logMsg = 'Fatal error: Failed to open DB connection.' +
-			  '\n' + err;
+			logMsg = 'ERR: Failed to open DB connection.\n' + err;
 			log.print(0, logMsg);
 			process.exit(1);
 		}
-		db.authenticate('user', 'password', function(err, result) {
-			if(err) {
-				console.error('Fatal error: Failed to ' +
-				  'authenticate to database.');
-				console.error(err);
-				process.exit(1);
-			}
-		});
+
+		openedDb(log, db, user, password);
+	});
+}
+
+function openedDb(log, db, user, password) {
+	db.authenticate('user', 'password', function(err, result) {
+		if(err) {
+			log.print(0, 'ERR: ' + err);
+			process.exit(1);
+		}
 	});
 }
 
