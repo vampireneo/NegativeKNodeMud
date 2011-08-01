f3f91a4ec881e6239ba761770a95c26c698e9db6
NegativeK <negativek+github@gmail.com>
2011-07-31 23:05:37 -0400

    Switch to callback for db.start.

--- server/index.js
+++ server/index.js
@@ -2,5 +2,6 @@ var log = require("./log");
 var server = require("./server");
 var db = require("./db");
 
-db.start(log);
-server.start(log, db);
+db.start(function() {
+	server.start(log, db.collections["users"]);
+});
