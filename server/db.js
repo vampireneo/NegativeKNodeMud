556357169b5efa0f6c95c40821eb7a13f154d715
NegativeK <negativek+github@gmail.com>
2011-07-31 23:04:53 -0400

    Try to add code for collections.

--- server/db.js
+++ server/db.js
@@ -1,35 +1,91 @@
 var mongo = require("mongodb");
+var log = require("./log");
 
+var db, user, password;
+var exportedDb, collections;
 
-function start(log) {
-	var db, user, password
+collections = new Array();
 
-	// Herp derp conf file.
-	user = 'user';
-	password = 'password';
-	db = new mongo.Db('mud', new mongo.Server("127.0.0.1", 27017, 
-	  {auto_reconnect:true}), {});
+// Herp derp conf file.
+user = "user";
+password = "password";
+collectionNames = ["users", "areas", "rooms", "mobs"];
+
+function start(serverCallback) {
+	db = new mongo.Db("mud", new mongo.Server("127.0.0.1", 27017, 
+	{auto_reconnect:true}), {});
 
 	db.open(function(err, db) {
 		if(err) {
 			var logMsg;
 
-			logMsg = 'ERR: Failed to open DB connection.\n' + err;
+			logMsg = "ERR: Failed to open DB connection.\n" + err;
 			log.print(0, logMsg);
-			process.exit(1);
 		}
 
-		openedDb(log, db, user, password);
+		openedDb(user, password, serverCallback);
 	});
 }
 
-function openedDb(log, db, user, password) {
-	db.authenticate('user', 'password', function(err, result) {
+function openedDb(user, password, serverCallback) {
+	db.authenticate(user, password, function(err, result) {
 		if(err) {
-			log.print(0, 'ERR: ' + err);
-			process.exit(1);
+			log.print(0, "ERR: " + err);
+		}
+		else if(!result) {
+			log.print(0, "ERR: Failed to authenticate to database.");
+		}
+
+		authedDb(result, serverCallback);
+	});
+}
+
+function authedDb(err, result, serverCallback) {
+	//gatherCollections(collectionNames, collections, serverCallback);
+	db.collections(function (err, collectionRefs) {
+		collections = collectionRefs;
+
+		exportExports(collections);
+		serverCallback();
+	});
+}
+
+/*
+function gatherCollections(oldNameList, collections, serverCallback) {
+	var name, nameList;
+
+	nameList = oldNameList;
+	name = nameList.pop();
+
+	db.collection(name, function(err, newCollection) {
+		if(err) {
+			var logMsg;
+
+			logMsg = "ERR: Failed to access collection.\n" + err;
+			log.print(0, logMsg);
+		}
+		else if(!newCollection) {
+			var logMsg;
+
+			logMsg = "ERR: Null collection returned.";
+			log.print(0, logMsg);
+		}
+
+		collections[name] = newCollection;
+
+		if(nameList.length > 0) {
+			gatherCollections(nameList, collections);
+		}
+		else {
+			exportExports(collections);
+			serverCallback(collections);
 		}
 	});
+}
+*/
+
+function exportExports(collections) {
+	exports.collections;
 }
 
 exports.start = start;
