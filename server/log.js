ff11981f83728735be1f2d8ce109707791291068
NegativeK <negativek+github@gmail.com>
2011-08-09 17:58:02 -0400

    Add message log level and error level handling

--- server/log.js
+++ server/log.js
@@ -1,8 +1,22 @@
-// Debug == 2, warn == 1, fatal == 0.
-var LOG_LEVEL = 2;
+// Debug == 3, message == 2, warn == 1, fatal == 0.
+var LOG_LEVEL = 3;
 
 function print(errorLevel, msg) {
+	// I hear that saving to a file might be a good idea -- especially for
+	// non-error messages.
 	if (errorLevel <= LOG_LEVEL) {
+		switch (errorLevel) {
+			case 3:
+				msg = "DBG: " + msg;
+				break;
+			case 1:
+				msg = "WRN: " + msg;
+				break;
+			case 0:
+				msg = "ERR: " + msg;
+				break;
+		}
+
 		console.error(msg);
 	}
 
@@ -11,6 +25,5 @@ function print(errorLevel, msg) {
 	}
 }
 
-// I hear that saving to a file might be a good idea, too!
 
 exports.print = print;
