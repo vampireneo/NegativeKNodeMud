349f30860de86b77b00e229da84af9beaf8c09a7
NegativeK <negativek+github@gmail.com>
2011-08-09 17:59:19 -0400

    Move input state machine to other file.

--- server/server.js
+++ server/server.js
@@ -1,47 +1,6 @@
 var websocket = require("websocket-server");
 
-function Machine(usersColl, log, connection) {
-	this.log = log;
-	this.usersColl = usersColl;
-	this.authenticated = false;
-	this.connection = connection;
-
-	this.welcome();
-}
-
-Machine.prototype.welcome = function() {
-	this.connection.send("Welcome to nodeMud. Beware of falling rocks.");
-	this.connection.send("Login: ");
-}
-
-Machine.prototype.parseInput = function(msg) {
-	// This might not be the best way to go about it. Think about having a
-	// nextFunction variable instead?
-	if (!this.authenticated) {
-		this.lookupUser(msg);
-	}
-	else {
-		this.connection.send(msg);
-	}
-}
-
-Machine.prototype.lookupUser = function(credentials) {
-	var valid;
-
-	valid = false;
-
-	this.usersColl.findOne(credentials.user, function(err, document) {
-		if(err) {
-			var logMsg;
-
-			logMsg = "Warning: Error looking up user '" + credentials.user +
-			  "' in database.\n" + err;
-			log(1, logMsg);
-		}
-	});
-}
-
-function start(log, usersColl) {
+function start(log, Machine, usersColl) {
 	var socketServer;
 
 	socketServer = websocket.createServer();
@@ -49,7 +8,9 @@ function start(log, usersColl) {
 	socketServer.addListener("connection", function(connection) {
 		var inputState;
 
-		inputState = new Machine(usersColl, log, connection);
+		inputState = new Machine(connection);
+
+		log.print(3, "Accepted connection from <IP>.");
 
 		connection.addListener("message", function(message) {
 			inputState.parseInput(message);
