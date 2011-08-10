159f758c4fa7fc137097154efa489a68c718f36b
NegativeK <negativek+github@gmail.com>
2011-08-10 12:44:11 -0400

    Mobs, not objs.

--- server/mongo/importScript.sh
+++ server/mongo/importScript.sh
@@ -2,4 +2,4 @@
 mongoimport --db mud --collection users --file dump/users.json
 mongoimport --db mud --collection rooms --file dump/rooms.json
 mongoimport --db mud --collection areas --file dump/areas.json
-mongoimport --db mud --collection objs --file dump/objs.json
+mongoimport --db mud --collection mobs --file dump/mobs.json
