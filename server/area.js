9a0dda462fe8f3d074c240a086045a2037d226a4
NegativeK <negativek+github@gmail.com>
2011-08-21 17:48:29 -0400

    Parameter conversion utility function and docs
    Wrote listToObj function.
    Wrote new and improved doumentation for Area and Mob.
    Updated Area and Mob to use listToObj.

--- server/area.js
+++ server/area.js
@@ -1,8 +1,40 @@
-function Area(name, minLevel, maxLevel, minVNum, maxVNum, areaBuilder) {
-	this.areaName = name;
-	this.levelRange = [minLevel, maxLevel];
-	this.builder = builder;
-	this.vNumRange = [minVNum, maxVNum];
+/* Parameters
+ *	paramList: Array; Strings for parameter names.
+ *	params: Passed object containing only parameters.
+ */
+var listToObj(paramList, params) {
+	var objReturn;
+
+	paramList.forEach(function(value) {
+		if (params[value] === undefined) {
+			throw("Object parameter " + value + " is undefined.");
+		}
+		else {
+			objReturn[value] = params[value];
+		}
+	});
+
+	return(objReturn);
+}
+
+/* Parameters (Object)
+ *	areaName: String; Area name, as will be visible by the area command.
+ *	filename: String; Filename for the area to be saved in.
+ *	levelRange: Array; Suggested minimum and maximum levels for the area.
+ *	vNumRange: Array: Range of area's vnums.
+ *	builder: String; Name of the area's builder.
+ */
+function Area(params) {
+	var paramList;
+
+	paramList = [
+					"areaName", "filename"
+				  , "levelRange", "builder"
+				  , "vNumRange"
+				];
+
+// This won't work if you want methods.
+	this = listToObj(paramList);
 }
 
 /* Parameters
@@ -20,24 +52,32 @@ function Area(name, minLevel, maxLevel, minVNum, maxVNum, areaBuilder) {
  *	level: Integer; Level of mob.
  *	hitBonus: Integer; Bonus/penalty (-2, -1, 0, 1, or 2) to hit for
  *		expert/crappy fighters.
- *	hitDice: Dice object.
- *	manaDice: Dice object.
- *	dmgDice: Dice object.
+ *	hitDice: Dice object for hit points.
+ *	manaDice: Dice object for mana points.
+ *	dmgDice: Dice object for damage.
  *	dmgType: String; Type of damage (description and mechanical type dealt to 
- *		attacked.
- *	
+ *		attacked.)
  */
 // Finish this parameter list.
 function Mob(paramObject) {
-	for (var property in paramNames) {
-		if (params.hasOwnProperty(property)) {
-			this[property] = paramObject[property];
-		}
-	} 
+	var paramList;
+
+	paramList = [
+					"nameList", "shortDesc"
+				  , "longDesc", "lookDesc"
+				  , "race", "actionFlags"
+				  , "affectFlags", "alignment"
+				  , "mobGroup", "level"
+				  , "hitBonus", "hitDice"
+				  , "manaDice", "dmgDice"
+				  , "dmgType"
+				];
+
+	this = listToObj(paramList);
 }
 
-function Object() {
-}
+//function Object() {
+//}
 
 function Room(id) {
 	this.id = id;
