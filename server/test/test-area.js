9696c59b3b956057bd8ee3cce15c53f88a224c6f
NegativeK <negativek+github@gmail.com>
2011-08-23 23:12:11 -0400

    Attempt to move to generic getters/setters

--- server/test/test-area.js
+++ server/test/test-area.js
@@ -1,7 +1,83 @@
 var area = require("../area.js");
 
 var areaTest = function(test) {
-	var anArea;
+	var anArea, areaFunctions, index;
+
+	areaFunctions = [
+					  "setBuilder"
+					, "setAreaName"
+					, "setFilename"
+					, "setVNumRange"
+					, " setLevelRange"
+					];
+
+	anArea = area.areaConstructor({
+		  "builder": "A Builder"
+		, "areaName": "Scary Area"
+		, "filename": "scary.are"
+		, "vNumRange": [1, 20]
+		, "levelRange": [3, 15]
+	});
+	
+	test.strictEqual(anArea.builder, "A Builder");
+	test.strictEqual(anArea.areaName, "Scary Area");
+	test.strictEqual(anArea.filename, "scary.are");
+	test.deepEqual(anArea.vNumRange, [1, 20]);
+	test.deepEqual(anArea.levelRange, [3, 15]);
+
+	anArea.builder = "newBuilder";
+	anArea.areaName = "Tasty Area";
+	anArea.filename = "tasty.are";
+	anArea.vNumRange = [6, 7];
+	anArea.levelRange = [40, 45];
+
+/*
+	for (index = 0; index < areaFunctions.length; index++) {
+		test.throws(function() {
+			var aFunction;
+
+			aFunction = areaFunctions[index];
+
+			anArea[aFunction]();
+		});
+	}
+
+	test.throws(function() {
+		anArea.setBuilder([5, 1]);
+	});
+	test.throws(function() {
+		anArea.setAreaName({});
+	});
+	test.throws(function() {
+		anArea.setFilename(4);
+	});
+	test.throws(function() {
+		anArea.setVNumRange("bleh");
+	});
+	test.throws(function() {
+		anArea.setLevelRange({});
+	});
+*/
+
+	test.strictEqual(anArea.builder, "newBuilder");
+	test.strictEqual(anArea.areaName, "Tasty Area");
+	test.strictEqual(anArea.filename, "tasty.are");
+	test.deepEqual(anArea.vNumRange, [6, 7]);
+	test.deepEqual(anArea.levelRange, [40, 45]);
+
+	test.done();
+};
+/*
+var areaTest = function(test) {
+	var anArea, areaFunctions, index;
+
+	areaFunctions = [
+					  "setBuilder"
+					, "setAreaName"
+					, "setFilename"
+					, "setVNumRange"
+					, " setLevelRange"
+					];
 
 	anArea = area.areaConstructor({
 		  "builder": "A Builder"
@@ -23,21 +99,15 @@ var areaTest = function(test) {
 	anArea.setVNumRange([6, 7]);
 	anArea.setLevelRange([40, 45]);
 
-	test.throws(function() {
-		anArea.setBuilder();
-	});
-	test.throws(function() {
-		anArea.setAreaName();
-	});
-	test.throws(function() {
-		anArea.setFilename();
-	});
-	test.throws(function() {
-		anArea.setVNumRange();
-	});
-	test.throws(function() {
-		anArea.setLevelRange();
-	});
+	for (index = 0; index < areaFunctions.length; index++) {
+		test.throws(function() {
+			var aFunction;
+
+			aFunction = areaFunctions[index];
+
+			anArea[aFunction]();
+		});
+	}
 
 	test.throws(function() {
 		anArea.setBuilder([5, 1]);
@@ -63,5 +133,6 @@ var areaTest = function(test) {
 
 	test.done();
 };
+*/
 
 exports.areaTest = areaTest;
