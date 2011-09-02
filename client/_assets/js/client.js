1fc3fd2857a14657110bdff6b53bb9c9092e23cb
Peter Mallett <peter.mallett@gmail.com>
2011-09-01 21:17:24 -0400

    Changed the terminal plugin to jqueryTerminal.
    http://terminal.jcubic.pl/

--- client/_assets/js/client.js
+++ client/_assets/js/client.js
@@ -2,35 +2,29 @@
 	$(document).ready(function(){
 		var serverSocket = new WebSocket("ws:localhost:4000");
 
-		var jQueryConsole = $('<div class="console">');
+		var jQueryTerminal = $('<div id="terminal">');
 
-		var controller = jQueryConsole.console({
-			promptLabel: 'nodeMud> ',
-			continuedPromptLabel: '  -> ',
-			commandHandle: function(line, report) {
-				if (line != '') {
-					serverSocket.send(line);
-				}
-				return true;
-			},
-			autofocus: true,
-			animateScroll: true,
-			promptHistory: true
+		$('body').append(jQueryTerminal);
+
+		$('#terminal').terminal(function(command, term) {
+			serverSocket.send(command);
+
+			serverSocket.onopen = function(event) {
+				term.echo('Connection opened.');
+			};
+
+			serverSocket.onmessage = function(event) {
+				term.echo(event.data);
+			};
+
+			serverSocket.onclose = function(event) {
+				term.echo("Connection closed: " + event.data);
+			};
+		}, {
+			name: 'nodeMud',
+			height: 600,
+			prompt: 'nodeMUD>'
 		});
-
-		serverSocket.onopen = function(event) {
-			controller.commandResult('Connection opened.');
-		};
-
-		serverSocket.onmessage = function(event) {
-			controller.commandResult(event.data);
-		};
-
-		serverSocket.onclose = function(event) {
-			controller.commandResult("Connection closed: " + event.data);
-		};
-
-		$('body').append(jQueryConsole);
 	});
 })(jQuery);
 
