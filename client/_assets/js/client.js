0c3a97abfbfc630b287da5590ed4b80350ab7016
Peter Mallett <peter.mallett@gmail.com>
2011-09-01 22:29:50 -0400

    Fix server connection and welcome message output.

--- client/_assets/js/client.js
+++ client/_assets/js/client.js
@@ -1,29 +1,30 @@
 ;(function($) {
 	$(document).ready(function(){
-		var serverSocket = new WebSocket("ws:localhost:4000");
+		var serverSocket= new WebSocket("ws:localhost:4000");
 
-		var jQueryTerminal = $('<div id="terminal">');
-
-		$('body').append(jQueryTerminal);
+		$('body').append('<div id="terminal">');
 
 		$('#terminal').terminal(function(command, term) {
 			serverSocket.send(command);
-
-			serverSocket.onopen = function(event) {
-				term.echo('Connection opened.');
-			};
-
-			serverSocket.onmessage = function(event) {
-				term.echo(event.data);
-			};
-
-			serverSocket.onclose = function(event) {
-				term.echo("Connection closed: " + event.data);
-			};
 		}, {
 			name: 'nodeMud',
 			height: 600,
-			prompt: 'nodeMUD>'
+			prompt: 'nodeMUD>',
+			onInit: function(term) {
+				term.clear();
+
+				serverSocket.onopen = function(event) {
+					term.echo('Connection opened.');
+				};
+
+				serverSocket.onmessage = function(event) {
+					term.echo(event.data);
+				};
+
+				serverSocket.onclose = function(event) {
+					term.echo("Connection closed: " + event.data);
+				};
+			}
 		});
 	});
 })(jQuery);
