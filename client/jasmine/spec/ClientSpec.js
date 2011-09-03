51711f90b5945cd869f9c420c2d8389a6a7109fc
Peter Mallett <peter.mallett@gmail.com>
2011-09-03 19:04:33 -0400

    Add jasmine testing framework ver 1.1.0.

---
+++ client/jasmine/spec/ClientSpec.js
@@ -0,0 +1,17 @@
+describe("Client", function() {
+  var client;
+
+  beforeEach(function() {
+    player = new Player();
+    song = new Song();
+  });
+
+  it("should ask for credentials when challenged", function() {
+  });
+
+  describe("when connection has been established", function() {
+    it("should do something", function() {
+    });
+  });
+});
+
