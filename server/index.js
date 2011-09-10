945a28ecb9a5748b683fd37b08560aa6c5ce4e32
NegativeK <negativek+github@gmail.com>
2011-09-10 16:04:13 -0400

    Style cleanup.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
-var log = require("./log");
-var db = require("./db");
-var parser = require("./parser");
-var server = require("./server");
+/*jslint node: true, white: true, plusplus: true, maxerr: 50, indent: 4 */
+"use strict";
+
+var log, db, parser, server;
+
+log = require("./log");
+db = require("./db");
+parser = require("./parser");
+server = require("./server");
 
 db.start(function() {
 	parser.start(db);
